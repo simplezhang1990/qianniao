58753aa30fd7f80280fcf0a360efe9c7afa7ec57
Zhang <Baiming.Zhang@Emerson.com>
2016-04-06 08:53:27 +0800

    add new file

--- app/src/main/java/com/simple/qianniao/LoginActivity.java
+++ app/src/main/java/com/simple/qianniao/LoginActivity.java
@@ -1,13 +1,15 @@
 package com.simple.qianniao;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends SimgleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+    protected Fragment createFragment() {
+        return new LoginFragment();
     }
+
+
 }
---
+++ app/src/main/java/com/simple/qianniao/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.simple.qianniao;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends SimgleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+
+   
+}
