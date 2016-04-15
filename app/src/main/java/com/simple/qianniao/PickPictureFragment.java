43fe36938d07ffdcf55f819ae1dec607bea160f7
Zhang <Baiming.Zhang@Emerson.com>
2016-04-15 16:12:21 +0800

    update take picture

--- app/src/main/java/com/simple/qianniao/PickPictureFragment.java
+++ app/src/main/java/com/simple/qianniao/PickPictureFragment.java
@@ -1,29 +1,47 @@
 package com.simple.qianniao;
 
-import android.content.Context;
-import android.graphics.Point;
-import android.util.Log;
-import android.view.WindowManager;
-import android.view.animation.Animation;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.TranslateAnimation;
 import android.widget.Button;
-import android.widget.LinearLayout;
+
+import com.simple.qianniao.activity.MainPageFragment;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Baiming.Zhang on 4/14/2016.
  */
 public class PickPictureFragment extends Fragment {
     private static final String TAG = "PickPictureFragment";
+    public static final int TAKE_PHOTO = 1;
+    public static final int PICK_PHOTO = 2;
     private Button mTakePicture;
     private Button mPickePicture;
-    private LinearLayout mLinearLayout;
+    private Uri mImageUri;
+    private String mFilename;
+    private String mCurrentUsername;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCurrentUsername = getActivity().getIntent().getStringExtra(MainPageFragment.SHAREDPREFERENCES_CURRENTUSER);
+    }
 
     @Nullable
     @Override
@@ -31,30 +49,50 @@ public class PickPictureFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_pick_picture, container, false);
         mTakePicture = (Button) view.findViewById(R.id.personal_take_picture);
         mPickePicture = (Button) view.findViewById(R.id.personal_pick_picture);
-        mLinearLayout = (LinearLayout) view.findViewById(R.id.personal_pick_picture_linearlayout);
         mTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
+                Date date = new Date(System.currentTimeMillis());
+                mFilename = format.format(date);
+                String externalStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+                String pachage = getContext().getPackageName();
+                File path = new File(externalStoragePath + "//" + pachage + "//" + mCurrentUsername);
+                if (!path.exists()) {
+                    path.mkdir();
+                }
+                File outputImage = new File(path, mFilename + ".jpg");
+                try {
+                    if (outputImage.exists()) {
+                        outputImage.delete();
+                    }
+                    outputImage.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                //将File对象转换为Uri并启动照相程序
+                mImageUri = Uri.fromFile(outputImage);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); //照相
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri); //指定图片输出地址
+                startActivityForResult(intent, TAKE_PHOTO);
 
-                Log.i(TAG,"   Linear height:" + mLinearLayout.getHeight());
+            }
+        });
+        mPickePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
         return view;
     }
 
-    private Animation initAnimation() {
-        WindowManager wm = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
-        Point point = new Point();
-        wm.getDefaultDisplay().getSize(point);
-        Animation animation = new TranslateAnimation(0, 0, 212, 0);
-        animation.setInterpolator(new LinearInterpolator());
-        animation.setDuration(500);
-        return animation;
-    }
-
     @Override
-    public void onResume() {
-        super.onResume();
-        mLinearLayout.startAnimation(initAnimation());
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TAKE_PHOTO && resultCode == getActivity().RESULT_OK) {
+            Log.i(TAG, "image path is " + mImageUri.getPath());
+            getActivity().finish();
+        }
     }
 }
--- app/src/main/java/com/simple/qianniao/activity/PersonalInfoFragment.java
+++ app/src/main/java/com/simple/qianniao/activity/PersonalInfoFragment.java
@@ -2,15 +2,23 @@ package com.simple.qianniao.activity;
 
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.simple.qianniao.R;
+import com.simple.qianniao.modul.Customer;
+import com.simple.qianniao.modul.CustomerSQLiteHelper;
 import com.simple.qianniao.view.UITableView;
 
 /**
@@ -20,13 +28,20 @@ public class PersonalInfoFragment extends Fragment {
 
     private UITableView mPersonInfoUITableView;
     private Button mPersonalTakePicture;
+    private CustomerSQLiteHelper mCustomerSQLiteHelper;
+    private SharedPreferences mSharedPreferences;
+    private String mSharedPreference_Username;
+    private Customer mCurrentCustomer;
+    private TextView mAlis, mAccount;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_personal_info, container, false);
         mPersonInfoUITableView = (UITableView) view.findViewById(R.id.personinfo_UITableView);
-        mPersonalTakePicture = (Button)view.findViewById(R.id.personal_upload_headimage_button);
+        mPersonalTakePicture = (Button) view.findViewById(R.id.personal_upload_headimage_button);
+        mAlis = (TextView) view.findViewById(R.id.personal_alis);
+        mAccount = (TextView) view.findViewById(R.id.personal_account);
         mPersonInfoUITableView.addBasicItem("example1");
         mPersonInfoUITableView.addBasicItem("example2", "summary");
 
@@ -36,12 +51,30 @@ public class PersonalInfoFragment extends Fragment {
         mPersonalTakePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(),PickPictureActivity.class);
+                Intent intent = new Intent(getContext(), PickPictureActivity.class);
+                intent.putExtra(MainPageFragment.SHAREDPREFERENCES_CURRENTUSER,mSharedPreference_Username);
                 startActivity(intent);
             }
         });
+        if (mCurrentCustomer != null) {
+            mAccount.setText(getResources().getString(R.string.register_username)+": "  +mCurrentCustomer.getUsername());
+            mAlis.setText(getResources().getString(R.string.register_alis)+": "  +mCurrentCustomer.getAlia());
+        }else {
+            Toast.makeText(getContext(),"customer info is null",Toast.LENGTH_SHORT).show();
+        }
+        if (!"".equals(mCurrentCustomer.getHeadImage())) {
 
+        }
         return view;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCustomerSQLiteHelper = CustomerSQLiteHelper.get(getContext());
+        mSharedPreferences = getActivity().getSharedPreferences(MainPageFragment.SHAREDPREFERENCES_NAME, Context.MODE_PRIVATE);
+        mSharedPreference_Username = mSharedPreferences.getString(MainPageFragment.SHAREDPREFERENCES_CURRENTUSER, "");
+        mCurrentCustomer = mCustomerSQLiteHelper.searchCustomer(mSharedPreference_Username);
+
+    }
 }
